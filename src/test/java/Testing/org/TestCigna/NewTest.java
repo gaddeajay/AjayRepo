0ef97fb76983d08b207ee46f31f370a788d8fa6a
Ajay Kumar Gadde <ajay.kumar.gadde@accenture.com>
2019-05-21 23:29:35 +0530

    Jenkins Parameter

--- src/test/java/Testing/org/TestCigna/NewTest.java
+++ src/test/java/Testing/org/TestCigna/NewTest.java
@@ -8,6 +8,8 @@ public class NewTest {
 	  System.out.println("In Calss  NewTest Method TestMethod");
 	  try
 	  {
+		  
+		  System.out.println("TestName Parameter From Jenkins = "+ System.getenv("TestName"));
 		  System.out.println("In Test Method");
 	  }
 	  catch(Exception e)
